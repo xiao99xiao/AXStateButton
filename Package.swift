db2c90c3643a2acc4a204a44d1c5e67b8ddde824
Xiao Xiao <xx2004xiamen@gmail.com>
2021-02-15 14:38:23 +0900

    Try add bridging header

--- Package.swift
+++ Package.swift
@@ -23,8 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AXStateButton",
-            path: "Source",
-            publicHeadersPath: "Source"
+            path: "Source"
         )
     ]
 )
---
+++ Source/AXStateButton-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Header.h
+//  
+//
+//  Created by Xiao Xiao on 2021/02/15.
+//
+
+#import "AXStateButton.h"
